7a95568ae0fa325ec453768f44ccab08cdebcd7f
mushihuahua <joeymqniac@gmail.com>
2024-01-09 01:05:56 +0000

    search: fixed problem where some recipes didnt load and made it so that you can load multiple pages at the press of the "load more" button

--- app/src/main/java/com/example/ecochef/screens/SearchScreen.kt
+++ app/src/main/java/com/example/ecochef/screens/SearchScreen.kt
@@ -1,12 +1,8 @@
 package com.example.ecochef.screens
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,21 +10,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.Surface
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -41,17 +37,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.toSize
 import coil.compose.AsyncImage
 import com.example.ecochef.R
 import com.example.ecochef.getIngredientsList
@@ -61,16 +57,15 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 
+var page = 1
 @Composable
 fun SearchScreen(componentActivity: ComponentActivity){
 
     var recipeSelectedHandler = remember { mutableStateOf<Boolean>(false) }
-
     var recipeSelected = remember { mutableStateOf<Recipe?>(null) }
+    var recipes = remember { mutableStateOf<Map<Int, List<Recipe>>>(emptyMap()) }
 
-    var urls = mutableListOf<String>()
-
-    var recipes by remember { mutableStateOf<List<Recipe>>(emptyList()) }
+    var isLoading by remember { mutableStateOf(false) }
 
     var ingredientNames = ArrayList<String>()
     for (ingredient in getIngredientsList()) {
@@ -80,12 +75,16 @@ fun SearchScreen(componentActivity: ComponentActivity){
             ingredientNames.add(ingredient.Iname)
         }
     }
+
     var urlString = "https://www.bbc.co.uk/food/search?q="
     ingredientNames.forEach {
         urlString = "$urlString$it%2C+"
     }
+    urlString = urlString.dropLast(4)
     Log.d("SearchDebug", "$urlString")
-    urls.add(urlString)
+
+    LoadNextRecipePage(urlString, recipes)
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -97,70 +96,133 @@ fun SearchScreen(componentActivity: ComponentActivity){
                 .verticalScroll(rememberScrollState())
                 .background(colorResource(id = R.color.mint_white))
         ) {
-//                    IngredientButtons(ingredients)
-            LaunchedEffect(Unit) {
-                MainScope().launch {
-                    withContext(Dispatchers.IO) {
-                        val fetchedRecipes = GetRecipe(urls)
-                        // Update the recipes on the main thread
-                        withContext(Dispatchers.Main) {
-                            recipes = fetchedRecipes
-                        }
-                    }
-                }
+
+            val finalRecipes = mutableListOf<Recipe>()
+
+            for (p in 1..page){
+                recipes.value[p]?.let { finalRecipes.addAll(it) }
             }
+
+
             if (!recipeSelectedHandler.value) {
 
-                for (i in recipes.indices step 2) {
+                if (finalRecipes != null) {
+                    for (i in finalRecipes.indices step 2) {
 
-                    var nextRecipe: Recipe? = null
+                        var nextRecipe: Recipe? = null
 
-                    var r: Recipe = recipes[i]
-                    if (i + 1 in recipes.indices) {
-                        nextRecipe = recipes[i + 1]
-                    }
+                        var r: Recipe = finalRecipes[i]
+                        if (i + 1 in finalRecipes.indices) {
+                            nextRecipe = finalRecipes[i + 1]
+                        }
 
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.Top,
-                        modifier = Modifier
-                            .padding(start = 4.dp, end = 4.dp, top = 8.dp, bottom = 8.dp)
-                            .fillMaxWidth()
-                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.Top,
+                            modifier = Modifier
+                                .padding(start = 4.dp, end = 4.dp, top = 8.dp, bottom = 8.dp)
+                                .fillMaxWidth()
+                        ) {
 
-                        RecipeItem(recipe = r, recipeSelectedHandler = recipeSelectedHandler, recipeSelected = recipeSelected)
+                            RecipeItem(
+                                recipe = r,
+                                recipeSelectedHandler = recipeSelectedHandler,
+                                recipeSelected = recipeSelected
+                            )
 
-                        if (nextRecipe != null) {
-                            RecipeItem(recipe = nextRecipe, recipeSelectedHandler = recipeSelectedHandler, recipeSelected = recipeSelected)
+                            if (nextRecipe != null) {
+                                RecipeItem(
+                                    recipe = nextRecipe,
+                                    recipeSelectedHandler = recipeSelectedHandler,
+                                    recipeSelected = recipeSelected
+                                )
+                            }
                         }
                     }
                 }
-            }
-            else {
-                recipeScreen(recipe = recipeSelected.value)
-            }
+                }
+                else {
+                    recipeScreen(recipe = recipeSelected.value)
+                }
 
-          //                  if(r.ingredients != null){
-        //                        for(ing in r.ingredients){
-      //                              Text(modifier = Modifier.padding(top = 2.dp) ,text = ing)
-    //                            }
-  //                          }
-//                            if(r.subRecipes != null){
-//                                for (recipe in r.subRecipes){
-//                                    Text(modifier = Modifier.padding(top = 20.dp) ,text = recipe.name, fontWeight = FontWeight.Bold)
-//                                    if(recipe.ingredients != null){
-//                                        for(subIng in recipe.ingredients){
-//                                            Text(modifier = Modifier.padding(top = 2.dp) ,text = subIng)
-//                                        }
-//                                    }
-//                                }
-//                            }
+                isLoading = false
+
+                if(recipes.value.isNotEmpty()){
+
+                    var buttonSize by remember { mutableStateOf(DpSize.Zero) }
+                    val density = LocalDensity.current
+
+                    if(recipes.value[page-1]?.isNotEmpty() == true){
+                        OutlinedButton(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .then(
+                                    if (buttonSize != DpSize.Zero) Modifier.size(buttonSize) else Modifier
+                                )
+                                .onSizeChanged { newSize ->
+                                    if (buttonSize == DpSize.Zero) {
+                                        buttonSize = with(density) {
+                                            newSize
+                                                .toSize()
+                                                .toDpSize()
+                                        }
+                                    }
+                                },
+                            onClick = { isLoading = isLoading.not() }) {
+                            if (isLoading) {
+                                CircularProgressIndicator(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .aspectRatio(1f)
+                                )
+                                LoadNextRecipePage(urlString, recipes)
+                            } else {
+                                Text(text = "More Recipes...")
+                            }
+                        }
+                    }
+                }
+//
+//              if(r.ingredients != null){
+//                for(ing in r.ingredients){
+//                    Text(modifier = Modifier.padding(top = 2.dp) ,text = ing)
+//                }
+//            }
+//            if(r.subRecipes != null){
+//                for (recipe in r.subRecipes){
+//                    Text(modifier = Modifier.padding(top = 20.dp) ,text = recipe.name, fontWeight = FontWeight.Bold)
+//                    if(recipe.ingredients != null){
+//                        for(subIng in recipe.ingredients){
+//                            Text(modifier = Modifier.padding(top = 2.dp) ,text = subIng)
+//                        }
+//                    }
+//                }
+//            }
 
         }
     }
 }
 
 data class Recipe(var name: String, val ingredients: List<String>?, val subRecipes: List<Recipe>?, val imageURL: String?)
+@Composable
+fun LoadNextRecipePage(url: String, recipes: MutableState<Map<Int, List<Recipe>>>){
+    LaunchedEffect(Unit) {
+        MainScope().launch {
+            withContext(Dispatchers.IO) {
+                var fetchedRecipes = recipes.value.toMutableMap()
+
+                val foundRecipes = GetRecipe("${url}&page=$page")
+                fetchedRecipes[page] = foundRecipes
+                page++
+
+                // Update the recipes on the main thread
+                withContext(Dispatchers.Main) {
+                    recipes.value = fetchedRecipes
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun RecipeItem(recipe: Recipe, modifier: Modifier = Modifier, recipeSelectedHandler: MutableState<Boolean>, recipeSelected: MutableState<Recipe?>){
@@ -175,7 +237,7 @@ fun RecipeItem(recipe: Recipe, modifier: Modifier = Modifier, recipeSelectedHand
             .clickable {
                 recipeSelectedHandler.value = true
                 recipeSelected.value = recipe
-                       },
+            },
     ) {
         Column {
 
@@ -215,55 +277,53 @@ fun RecipeItem(recipe: Recipe, modifier: Modifier = Modifier, recipeSelectedHand
     }
 }
 
-fun GetRecipe(urls: List<String>): MutableList<Recipe> {
+fun GetRecipe(url: String): MutableList<Recipe> {
     var recipes: MutableList<Recipe> = mutableListOf<Recipe>()
 
-    for (url in urls) {
-        try {
-            val document = Jsoup.connect(url).get()
+    try {
+        val document = Jsoup.connect(url).get()
 
-            val recipeElements = document.select(".promo__main_course")
+        val recipeElements = document.select(".promo")
 
-            for (element in recipeElements) {
-                val recipeImage = element.selectFirst("div.promo__image")?.selectFirst("img")?.absUrl("data-src")
-                val recipeURL = element.absUrl("href")
-                val recipeDocument = Jsoup.connect(recipeURL).get()
+        for (element in recipeElements) {
+            val recipeImage = element.selectFirst("div.promo__image")?.selectFirst("img")?.absUrl("data-src")
+            val recipeURL = element.absUrl("href")
+            val recipeDocument = Jsoup.connect(recipeURL).get()
 
-                val ingredientsElements = recipeDocument.select("div.recipe-ingredients-wrapper")
+            val ingredientsElements = recipeDocument.select("div.recipe-ingredients-wrapper")
 
-                val subRecipes = mutableListOf<Recipe>()
+            val subRecipes = mutableListOf<Recipe>()
 
-                var subRecipeName = String()
-                var ingredients = listOf<String>()
+            var subRecipeName = String()
+            var ingredients = listOf<String>()
 
-                for (elem in ingredientsElements[0].children()){
-                    var subIngredients = listOf<String>()
+            for (elem in ingredientsElements[0].children()){
+                var subIngredients = listOf<String>()
 
-                    if(elem.tagName().equals("h3")){
-                        subRecipeName = elem.text()
-                    }
-
-                    if(elem.tagName().equals("ul") && subRecipeName == ""){
-                        ingredients = elem.select("li")
-                            .map { it.text() }
-                    }
-
-                    if(elem.tagName().equals("ul") && subRecipeName != ""){
-                        subIngredients = elem.select("li")
-                            .map { it.text() }
-
-                        subRecipes.add(Recipe(subRecipeName, subIngredients, null, null))
-
-                        subRecipeName = ""
-                        subIngredients = listOf()
-                    }
+                if(elem.tagName().equals("h3")){
+                    subRecipeName = elem.text()
                 }
-                recipes.add(Recipe(recipeDocument.select("h1.content-title__text").text(), ingredients, subRecipes, recipeImage))
-            }
 
-        } catch (e: Exception) {
-            e.printStackTrace()
+                if(elem.tagName().equals("ul") && subRecipeName == ""){
+                    ingredients = elem.select("li")
+                        .map { it.text() }
+                }
+
+                if(elem.tagName().equals("ul") && subRecipeName != ""){
+                    subIngredients = elem.select("li")
+                        .map { it.text() }
+
+                    subRecipes.add(Recipe(subRecipeName, subIngredients, null, null))
+
+                    subRecipeName = ""
+                    subIngredients = listOf()
+                }
+            }
+            recipes.add(Recipe(recipeDocument.select("h1.content-title__text").text(), ingredients, subRecipes, recipeImage))
         }
+
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 
     return recipes
